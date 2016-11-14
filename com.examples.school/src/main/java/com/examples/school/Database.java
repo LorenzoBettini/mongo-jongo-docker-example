e77af0428a2d344f923ee497268569ee7093d5e9
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2016-11-14 11:46:48 +0100

    added some classes and tests

---
+++ com.examples.school/src/main/java/com/examples/school/Database.java
@@ -0,0 +1,12 @@
+package com.examples.school;
+
+import java.util.List;
+
+public interface Database {
+
+	public List<Student> getAllStudentsList();
+
+	public void updateDB(String id, String name);
+
+	public boolean exists(String id);
+}
